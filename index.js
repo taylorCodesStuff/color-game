3492914aa3a6f3041e88852c0a0fad288d5e10c6
Taylor Hammons <taylor@thinkful.com>
2018-07-18 19:08:18 -0500

    added game mode btns and display functionality

--- index.js
+++ index.js
@@ -4,18 +4,53 @@ let randomNum = Math.floor((Math.random() * 10) + 1);
 
 
 let state = {
-	gamemode: 'easy',
-	numOfSquares: 9
+	numOfSquares: 9,
+	mode: 'Impossible'
 }
 
 init();
 
 function init(){
 	handleSquareClickEvent();
+	handleGameMode();
 }
 
 function handleSquareClickEvent(){
 	$('.square').on('click', function(event){
 		// check if correct
 	});
+}
+
+function handleGameMode(){
+	$('.mode').on('click', function(event){
+		console.log('mode clicked');
+		console.log(state.mode);
+		$('.mode').removeClass('selected');
+		$(this).addClass('selected');
+		state.mode = $(this).text();
+		console.log(state.mode);
+		determineNumOfSquares(state.mode);
+		displaySquares();
+	});
+}
+
+function determineNumOfSquares(gamemode){
+	if(gamemode === 'Easy'){
+		state.numOfSquares = 3;
+	} else if(gamemode === 'Hard'){
+		state.numOfSquares = 6;
+	} else if(gamemode === 'Harder' || gamemode === 'Impossible'){
+		state.numOfSquares = 9;
+	}
+}
+
+function displaySquares(){
+	if(state.numOfSquares === 3){
+		$('.easy').hide();
+	} else if(state.numOfSquares === 6){
+		$('.square').show();
+		$('.hard').hide();
+	} else {
+		$('.square').show();
+	}
 }
